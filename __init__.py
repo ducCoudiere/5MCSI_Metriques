08225265ee1e5bcf122b09ff45e4575aa86f1ef0
ducCoudiere <165167098+ducCoudiere@users.noreply.github.com>
2024-06-07 11:27:32 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,9 +11,13 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
+# @app.route("/contact/")
+# def MaPremiereAPI():
+    # return "<h2>Ma page de contact</h2>"
+
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+    return render_template("formulaire.html")
 
 @app.route('/tawarano/')
 def meteo():
