106e58f305ef3b019912b91bb2015f080fe95fb5
ducCoudiere <165167098+ducCoudiere@users.noreply.github.com>
2024-06-07 10:35:50 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+                                                                                                                                        
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
